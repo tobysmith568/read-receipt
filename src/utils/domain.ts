a9e15cac47a6a02d43e4304d488d15fcab60741f
Toby Smith <tobysmith568@hotmail.co.uk>
2022-03-07 00:33:13 +0000

    Use http during dev

--- src/utils/domain.ts
+++ src/utils/domain.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest } from "next";
+import env from "./env";
 
 export const getDomainForRequest = (req: NextApiRequest): string => {
+  const protocol = env.dev.isDev ? "http" : "https";
   const host = req.headers.host;
-  return `https://${host}`;
+  return `${protocol}://${host}`;
 };
